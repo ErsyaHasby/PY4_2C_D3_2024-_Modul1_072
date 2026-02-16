63de63975bd707ce5e3ca3eac9a10111e207f6a7
Ersya Hasby Satria <hasbysatria54@gmail.com>
2026-02-16 15:47:18 +0700

    Add modul 2

--- TASK0/lib/counter_controller.dart
+++
@@ -1,9 +0,0 @@
-class CounterController {
-  int _counter = 0; // Variabel private (Enkapsulasi)
-
-  int get value => _counter; // Getter untuk akses data
-
-  void increment() => _counter++;
-  void decrement() { if (_counter > 0) _counter--; }
-  void reset() => _counter = 0;
-}
--- TASK2/android/app/src/main/kotlin/com/example/logbook_srp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.logbook_srp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
